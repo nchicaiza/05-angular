4b1d8c1fbf05652300a8aafb7905c784ede32c1c
Adrian Eguez <eadepto@hotmail.com>
2018-06-25 10:04:39 -0500

    interpolacion

--- App/src/app/app.component.ts
+++ App/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [
+    './app.component.css'
+  ]
 })
 export class AppComponent {
-  title = 'app';
+  // Interpolacion
+  titulo = 'Web';
+  usuario = {
+    nombre: 'Adrian',
+    apellido: 'Eguez'
+  };
+  arregloNumeros = [5, 4, 3, 2, 1];
+  mostrarTitulo = true;
 }
+
